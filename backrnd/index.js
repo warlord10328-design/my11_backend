f2c55caedfe3e8504efb7f60be5c11483b40fe89
warlord10328-design <warlord10328@gmail.com>
2025-12-22 17:08:39 +0530

    Update index.js

--- backrnd/index.js
+++ backrnd/index.js
@@ -28,6 +28,4 @@ app.use("/insert", insertRoutes);
 app.use("/scrape", scrapeData);
 
 // -------------------- SERVER --------------------
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`🚀 Server running at http://localhost:${process.env.PORT || 5000}`);
-});
+export default app;
