6a317b8e5d257c186552e5d5ed26b12a566b2d3e
warlord10328-design <warlord10328@gmail.com>
2025-12-23 08:47:58 +0530

    Update app.js

--- backrnd/app.js
+++ backrnd/app.js
@@ -1,41 +1,33 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 
 import userMatchRoutes from "./controller/usermatch.js";
 import adminRoutes from "./auth/adminauth.js";
 import userRoutes from "./auth/userauth.js";
-import insertRoutes from "./auth/insert.js"; 
-import scrapeData from "./auth/scrapme.js"; 
+import insertRoutes from "./auth/insert.js";
+import scrapeData from "./auth/scrapme.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true,
+    origin: "https://my11-admin-run.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 // -------------------- ROUTES --------------------
-app.use("/", userRoutes);             // /me, /login, /logout
-app.use("/match", userMatchRoutes); // /matches, /matches/:id
+app.use("/", userRoutes);
+app.use("/match", userMatchRoutes);
 app.use("/admin", adminRoutes);
 app.use("/insert", insertRoutes);
 app.use("/scrape", scrapeData);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "Backend running 🚀" });
+  res.json({ status: "Backend running 🚀" });
 });
 
-// -------------------- SERVER --------------------
 export default app;
-
-
-
-
