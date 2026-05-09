a68374cf2d77168ed13cf99fd4a97e77cda9af04
warlord10328-design <warlord10328@gmail.com>
2026-05-09 15:56:45 +0530

    Update app.js

--- backrnd/app.js
+++ backrnd/app.js
@@ -2,6 +2,16 @@ import express from "express";
 import cors from "cors";
 
 import adminRoutes from "./admin/signin/adminauth.js";
+import userMatchRoutes from "./controller/usermatch.js";
+import userContest from "./user/user_contest.js";
+import userRoutes from "./auth/userauth.js";
+import adminMatch from "./admin/game_control/match.js";
+import dataInsert from "./admin/game_control/data_insert.js"; 
+import dataFetch from "./admin/game_control/data_fetch.js";
+import apiRun from "./admin/api/fetch.js";
+import jobRun from "./admin/api/job.js";
+import scrapeData from "./auth/scrapme.js"; 
+import userTeam from "./user/user_team_creation.js";
 
 
 const app = express();
@@ -20,7 +30,16 @@ app.use(
 // -------------------- ROUTES --------------------
 
 app.use("/admin", adminRoutes);
-
+app.use("/", userRoutes);
+app.use("/match", userMatchRoutes);
+app.use("/insert", dataInsert);
+app.use("/fetch", dataFetch);
+app.use("/scrape", scrapeData);
+app.use("/admin_match", adminMatch);
+app.use("/contests",userContest);
+app.use("/userteam",userTeam);
+app.use("/jobRun",jobRun);
+app.use("/apiRun",apiRun);
 
 
 
