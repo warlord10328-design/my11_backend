ef57b00dfc5bf5ca3817d3d25dbb2db67f430f69
warlord10328-design <warlord10328@gmail.com>
2026-05-09 14:14:32 +0530

    Update app.js

--- backrnd/app.js
+++ backrnd/app.js
@@ -2,17 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import userMatchRoutes from "./controller/usermatch.js";
 import adminSignin from "./admin/signin/adminauth.js";
-import userContest from "./user/user_contest.js";
-import userRoutes from "./auth/userauth.js";
-import adminMatch from "./admin/game_control/match.js";
-import dataInsert from "./admin/game_control/data_insert.js";
-import dataFetch from "./admin/game_control/data_fetch.js";
-import apiRun from "./admin/api/fetch.js";
-import jobRun from "./admin/api/job.js";
-import scrapeData from "./auth/scrapme.js";
-import userTeam from "./user/user_team_creation.js";
+
 
 const app = express();
 
@@ -33,16 +24,7 @@ app.use(cookieParser());
 
 // -------------------- ROUTES --------------------
 app.use("/", userRoutes);
-app.use("/match", userMatchRoutes);
 app.use("/admin", adminSignin);
-app.use("/insert", dataInsert);
-app.use("/fetch", dataFetch);
-app.use("/scrape", scrapeData);
-app.use("/admin_match", adminMatch);
-app.use("/contests", userContest);
-app.use("/userteam", userTeam);
-app.use("/jobRun", jobRun);
-app.use("/apiRun", apiRun);
 
 // health route
 app.get("/", (req, res) => {
