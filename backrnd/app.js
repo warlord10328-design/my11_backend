b5c7e722df26fa21dcaea89039b4fce1d762cca7
warlord10328-design <warlord10328@gmail.com>
2026-05-09 13:50:07 +0530

    Update app.js

--- backrnd/app.js
+++ backrnd/app.js
@@ -1,27 +1,47 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import userMatchRoutes from "./controller/usermatch.js";
-import adminRoutes from "./auth/adminauth.js";
+import adminSignin from "./admin/signin/adminauth.js";
+import userContest from "./user/user_contest.js";
 import userRoutes from "./auth/userauth.js";
-import insertRoutes from "./auth/insert.js";
-import scrapeData from "./auth/scrapme.js";
+import adminMatch from "./admin/game_control/match.js";
+import dataInsert from "./admin/game_control/data_insert.js"; 
+import dataFetch from "./admin/game_control/data_fetch.js";
+import apiRun from "./admin/api/fetch.js";
+import jobRun from "./admin/api/job.js";
+import scrapeData from "./auth/scrapme.js"; 
+import userTeam from "./user/user_team_creation.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://my11-admin-run.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 // -------------------- ROUTES --------------------
 app.use("/", userRoutes);
 app.use("/match", userMatchRoutes);
-app.use("/admin", adminRoutes);
-app.use("/insert", insertRoutes);
+app.use("/admin", adminSignin);
+app.use("/insert", dataInsert);
+app.use("/fetch", dataFetch);
 app.use("/scrape", scrapeData);
+app.use("/admin_match", adminMatch);
+app.use("/contests",userContest);
+app.use("/userteam",userTeam);
+app.use("/jobRun",jobRun);
+app.use("/apiRun",apiRun);
 
-app.get("/", (req, res) => {
-  res.json({ status: "Backend running 🚀" });
+// -------------------- SERVER --------------------
+const HOST = process.env.HOST || "localhost";
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`🚀 Server running at http://${HOST}:${process.env.PORT || 5000}`);
 });
-
-export default app;
