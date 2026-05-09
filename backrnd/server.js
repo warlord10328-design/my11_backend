e2c4931b4f6eaa6eca75ced47e0bece7747690a9
warlord10328-design <warlord10328@gmail.com>
2026-05-09 15:28:31 +0530

    Update server.js

--- backrnd/server.js
+++ backrnd/server.js
@@ -1,25 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import pool from "./db.js";
 
 dotenv.config();
 
 const app = express();
 
-// middleware
+
 app.use(express.json());
 
-app.use(cors({
-  origin: "https://my11-admin-run.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
-app.options("*", cors());
-
-// routes
 app.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
@@ -31,12 +20,14 @@ app.get("/", async (req, res) => {
 
   } catch (err) {
     console.error("Database error:", err);
+    res.status(500).json({ error: "Database connection failed" });
 
-    res.status(500).json({
-      error: "Database connection failed"
-    });
+
+
   }
 });
 
-// IMPORTANT
-export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
