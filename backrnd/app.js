d746a55091a86922ca268e0fdbcbc0d76445fc17
warlord10328-design <warlord10328@gmail.com>
2026-05-09 14:23:45 +0530

    Update app.js

--- backrnd/app.js
+++ backrnd/app.js
@@ -1,23 +1,9 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-  origin: "https://my11-admin-run.vercel.app",
-  credentials: true
-}));
-
-app.options("*", cors());
-
 app.get("/", (req, res) => {
-  res.send("working");
-});
-
-app.post("/admin/login", (req, res) => {
-  res.json({
-    success: true
-  });
+  res.send("Backend Working");
 });
 
 export default app;
